af2bce0a43547707a707a88caab308fa3db1b09d
jonato96 <jonato96s@gmail.com>
2024-10-11 20:53:56 -0500

    add-simple-controller-to-avoid-404-error

--- devs-on-deck-backend/src/main/java/com/artic/dev/controller/HelloController.java
+++ devs-on-deck-backend/src/main/java/com/artic/dev/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.artic.dev.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
-    public String hello() {
-        return "Hello from server";
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello from server for devs on deck.");
     }
 }
