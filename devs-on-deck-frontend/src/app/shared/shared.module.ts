d853f49e2691d21d5608b9cdb63735c2ec8b42c1
jonato96 <jonato96s@gmail.com>
2024-10-19 21:38:32 -0500

    add-custom-control-form

--- devs-on-deck-frontend/src/app/shared/shared.module.ts
+++ devs-on-deck-frontend/src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { InputTextModule } from "primeng/inputtext";
 
 import { CustomTopBarComponent } from './ui/components/custom-top-bar/custom-top-bar.component';
 import { CustomControlComponent } from './ui/components/custom-control/custom-control.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {InputTextModule} from "primeng/inputtext";
 
 
 @NgModule({
@@ -13,13 +13,13 @@ import {InputTextModule} from "primeng/inputtext";
     CustomTopBarComponent,
     CustomControlComponent
   ],
-    imports: [
-        CommonModule,
-        RouterLink,
-        FormsModule,
-        InputTextModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+      CommonModule,
+      RouterLink,
+      FormsModule,
+      InputTextModule,
+      ReactiveFormsModule
+  ],
   exports: [
     CustomTopBarComponent,
     CustomControlComponent
