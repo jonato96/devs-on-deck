23e6c77d60156cf9faf1cfe4f1cce5d1d97efb92
jonato96 <jonato96s@gmail.com>
2024-12-06 19:27:39 -0500

    add-update-imports

--- devs-on-deck-backend/src/main/java/com/artic/dev/entity/Framework.java
+++ devs-on-deck-backend/src/main/java/com/artic/dev/entity/Framework.java
@@ -1,6 +1,11 @@
 package com.artic.dev.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
