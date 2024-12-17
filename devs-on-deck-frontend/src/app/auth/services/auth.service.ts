e785381a44988b08fc156fad2ceb56818994546c
jonato96 <jonato96s@gmail.com>
2024-12-16 19:55:43 -0500

    add-format-code

--- devs-on-deck-frontend/src/app/auth/services/auth.service.ts
+++ devs-on-deck-frontend/src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { map, Observable } from "rxjs";
-import {Auth, AuthData, DeveloperData, LoginResponse, OrganizationData} from "@/auth/models";
+import { Auth, AuthData, DeveloperData, LoginResponse, OrganizationData } from "@/auth/models";
 import { AuthAdapter } from "@/auth/adapters/auth.adapter";
 
 @Injectable({
