967f2cf5e197d935b8d256e4740d1d103c1b24be
jonato96 <jonato96s@gmail.com>
2024-12-17 20:18:10 -0500

    add-format-code

--- devs-on-deck-frontend/src/app/org/org.module.ts
+++ devs-on-deck-frontend/src/app/org/org.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { OrgRoutingModule } from './org-routing.module';
 import { LayoutComponent } from './layout/layout.component';
-import {SharedModule} from "@/shared/shared.module";
+import { SharedModule } from "@/shared/shared.module";
 
 
 @NgModule({
