f8a4e9cf8555db96f5b3537a3aa3194726eb2679
jonato96 <jonato96s@gmail.com>
2025-01-08 20:18:25 -0500

    add-redirect-logout

--- devs-on-deck-frontend/src/app/shared/ui/components/custom-logged-top-bar/custom-logged-top-bar.component.ts
+++ devs-on-deck-frontend/src/app/shared/ui/components/custom-logged-top-bar/custom-logged-top-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, input } from '@angular/core';
 import { LocalManagerService } from "@/auth/services";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-custom-logged-top-bar',
@@ -9,10 +10,12 @@ import { LocalManagerService } from "@/auth/services";
 export class CustomLoggedTopBarComponent {
 
   private readonly managerService = inject(LocalManagerService);
+  private readonly router = inject(Router);
   title = input<string>();
 
   logout() {
     this.managerService.clearStorage();
+    void this.router.navigateByUrl('/auth/login/dev');
   }
 
 }
