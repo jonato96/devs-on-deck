8d596f0b5928d65b011dca8ae9e9c4f16d383743
jonato96 <jonato96s@gmail.com>
2025-01-24 21:06:41 -0500

    add-repositories

--- devs-on-deck-backend/src/main/java/com/artic/dev/service/impl/DeveloperSkillServiceImpl.java
+++ devs-on-deck-backend/src/main/java/com/artic/dev/service/impl/DeveloperSkillServiceImpl.java
@@ -2,6 +2,8 @@ package com.artic.dev.service.impl;
 
 import com.artic.dev.dto.SkillRequestDto;
 import com.artic.dev.repository.DeveloperLanguageFrameworkRepository;
+import com.artic.dev.repository.FrameworkRepository;
+import com.artic.dev.repository.LanguagesRepository;
 import com.artic.dev.service.DeveloperSkillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 public class DeveloperSkillServiceImpl implements DeveloperSkillService {
 
     private final DeveloperLanguageFrameworkRepository repository;
+    private final LanguagesRepository languagesRepository;
+    private final FrameworkRepository frameworkRepository;
 
     @Override
     public void createSkill(SkillRequestDto request) {
