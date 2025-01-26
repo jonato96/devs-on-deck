d02789f9fccfc5379fd2fdd87dd19ab7143eb462
jonato96 <jonato96s@gmail.com>
2025-01-25 20:20:00 -0500

    add-base-controller

--- devs-on-deck-backend/src/main/java/com/artic/dev/controller/DeveloperSkillController.java
+++ devs-on-deck-backend/src/main/java/com/artic/dev/controller/DeveloperSkillController.java
@@ -1,7 +1,10 @@
 package com.artic.dev.controller;
 
+import com.artic.dev.dto.SkillRequestDto;
 import com.artic.dev.service.DeveloperSkillService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeveloperSkillController {
 
     private final DeveloperSkillService service;
+
+    @GetMapping
+    public String saveSkill(@RequestBody SkillRequestDto request) {
+        this.service.createSkill(request);
+        return "";
+    }
 }
